7b2c060cfed2364f94f30eb956039d513a27ae67
Kelly Toomes <kelly@Kellys-MacBook.local>
2017-08-07 15:06:59 -0400

    content structure

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log(customers);
+
+for(let i = 0; i < customers.results.length; i++){
+  let customerData = customers.results[i];
+  var imageProfile = customerData.picture;
+  var name = customerData.name.first + " " + customerData.name.last
+  console.log(name);
+  var emailAddress = customerData.emailAddress;
+  console.log(emailAddress);
+  var telNumber = 
+
+let content =  `<li>
+      <div class = "person-info">
+        <h3>${imageProfile}</h3>
+        <h1>${name}</h1>
+        <p>${emailAddress}</p>
+        <p>${telNumber}</p>
+        <p>${something}</p>
+        <p>${sSNumber}</p>
+      </div>
+    </li>`
+}
