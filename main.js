afae8578d2acd108905868f775ce4bc515b61f1b
Kelly Toomes <kelly@Kellys-MacBook.local>
2017-08-22 06:38:08 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-
-for(let i = 0; i < customers.results.length; i++){
-  let customerData = customers.results[i];
-  var imageProfile = customerData.picture;
-  var name = customerData.name.first + " " + customerData.name.last
-  console.log(name);
-  var emailAddress = customerData.emailAddress;
-  console.log(emailAddress);
-  var telNumber = 
-
-let content =  `<li>
-      <div class = "person-info">
-        <h3>${imageProfile}</h3>
-        <h1>${name}</h1>
-        <p>${emailAddress}</p>
-        <p>${telNumber}</p>
-        <p>${something}</p>
-        <p>${sSNumber}</p>
-      </div>
-    </li>`
+console.log(customers);
+function list(){
+  for(let i = 0; i < 12; i++){
+    let customerData = customers.results[i];
+    let div = document.createElement('div')
+    let content = `
+        <img src = ${customerData.picture.large}>
+        <h2>${customerData.name.first} ${customerData.name.last}</h2>
+        <p>${customerData.location.street}</p>
+        <p>${customerData.location.city} ${customerData.location.state} ${customerData.location.postcode}</p>
+        <p>${customerData.email}</p>
+        <p>${customerData.phone}</p>
+        <p>${customerData.id.value}</p>
+      `;
+    div.innerHTML = content;
+    console.log(div);
+    document.getElementById("people").appendChild(div);
+  }
 }
+list();
